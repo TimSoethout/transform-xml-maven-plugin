832d2fd0700bf151498e7c9c75eaa52d4f2552a6
Tim Soethout <Tim.Soethout@ing.nl>
2015-06-01 10:58:58 +0200

    Quick delete implementation using javax.xml

--- src/main/scala/nl/timmybankers/maven/TransformXmlMojo.scala
+++ src/main/scala/nl/timmybankers/maven/TransformXmlMojo.scala
@@ -1,16 +1,15 @@
 package nl.timmybankers.maven
 
-import java.io.FileReader
-import javax.xml.parsers.{DocumentBuilderFactory, DocumentBuilder}
+import java.io.{FileWriter, StringWriter}
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.{Transformer, TransformerFactory}
 import javax.xml.xpath._
 
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.{Mojo, Parameter}
 import org.w3c.dom.{Document, Node, NodeList}
-import org.xml.sax.InputSource
-import scales.xml.ScalesXml._
-
-//import scales.xml._
 
 @Mojo(name = "transform-xml")
 class TransformXmlMojo extends AbstractMojo {
@@ -18,60 +17,60 @@ class TransformXmlMojo extends AbstractMojo {
   @Parameter(property = "inputXmlPath")
   var inputXmlPath: String = null
   @Parameter(property = "outputXmlPath")
-  val outputXmlPath: String = null
-//  @Parameter
-//  val xpath: String = null
-  @Parameter
-  val action: String = "DELETE"
+  var outputXmlPath: String = null
+  @Parameter(property = "xpath")
+  var xpath: String = null
+  @Parameter(property = "action")
+  var action: String = "DELETE"
 
   override def execute(): Unit = {
     getLog.debug("Starting Transform XML Maven plugin")
 
-//    val xmlDoc: InputSource = new FileReader(inputXmlPath)
+    val xmlDoc = loadFile(inputXmlPath)
 
-    val xpath: XPath = XPathFactory.newInstance().newXPath()
+    val xpathHandler: XPath = XPathFactory.newInstance().newXPath()
 
-    val factory = DocumentBuilderFactory.newInstance()
-    factory.setNamespaceAware( true );
-    val builder = factory.newDocumentBuilder();
-    builder.setEntityResolver( new BlankingResolver() );
-
-    val xmlDoc: Document = builder.parse(inputXmlPath)
-    val pathExpr: XPathExpression = xpath.compile("//class[contains(@filename,'.scala')]");
-    //    try {
-    //      pathExpr =
-    //    } catch {
-    //      case (e: XPathExpressionException) => e.printStackTrace();
-    //    }
+    val pathExpr: XPathExpression = xpathHandler.compile(xpath)
     val list: NodeList = pathExpr.evaluate(xmlDoc, XPathConstants.NODESET).asInstanceOf[NodeList]
-//    try {
-//      list =
-//    } catch {
-//      case (e: XPathExpressionException) => e.printStackTrace();
-//    }
 
-    for ( i <- 0 to list.getLength) {
+    for (i <- 0 until list.getLength) {
       val node: Node = list.item(i)
-      getLog.debug(s"Removing node: ${node}")
+      getLog.debug(s"Removing node: ${node.getNodeName}}")
       node.getParentNode.removeChild(node)
     }
 
-    getLog.info(xmlDoc.toString)
 
+    writeToFile(xmlDoc)
 
-    //    val xml: Doc = loadXml(new FileReader(inputXmlPath))
-
-    //    val path: ScalesXPath = ScalesXPath("//class[contains(@filename,'.scala')]")
-    //
-    //    val filter: Iterable[Either[AttributePath, XmlPath]] = path.evaluate(top(xml))
-    //
-    //    val output: String = asString(filter)
-    //    getLog.debug(output)
-    //    val writer: Writer = new FileWriter(outputXmlPath)
-    //    output.writeTo(new StringWriter())
-
-
-    //    inputFile.
     getLog.debug("Finished Transform XML Maven plugin")
+  }
+
+  def writeToFile(xmlDoc: Document): Unit = {
+    val source: DOMSource = new DOMSource(xmlDoc)
+    val transFactory: TransformerFactory = TransformerFactory.newInstance()
+    val transformer: Transformer = transFactory.newTransformer()
+    if (getLog.isDebugEnabled) {
+
+      val writer: StringWriter = new StringWriter()
+
+      transformer.transform(source, new StreamResult(writer))
+      writer.flush()
+      getLog.debug(writer.toString)
+    }
+
+    getLog.info("writing to file " + outputXmlPath)
+    transformer.transform(source, new StreamResult(new FileWriter(outputXmlPath)))
+  }
+
+  def loadFile(inputXmlPath: String): Document = {
+
+
+    val factory = DocumentBuilderFactory.newInstance()
+    factory.setNamespaceAware(true);
+    val builder = factory.newDocumentBuilder();
+    builder.setEntityResolver(new BlankingResolver());
+
+    val xmlDoc: Document = builder.parse(inputXmlPath)
+    xmlDoc
   }
 }
--- src/test/scala/nl/timmybankers/maven/TransformXmlMojoTest.scala
+++ src/test/scala/nl/timmybankers/maven/TransformXmlMojoTest.scala
@@ -1,18 +1,33 @@
 package nl.timmybankers.maven
 
+import java.io.File
+import java.nio.file.{Files, Paths}
+
 import org.scalatest._
 
-class TransformXmlMojoTest extends FlatSpec {
+import scala.xml.{NodeSeq, XML}
+
+class TransformXmlMojoTest extends FlatSpec with ShouldMatchers {
 
   "execte" should "work with some sample xml" in {
 
     val mojo: TransformXmlMojo = new TransformXmlMojo
-    mojo.inputXmlPath = ""
-//    mojo.outputXmlPath = ""
+    mojo.inputXmlPath = Paths.get(getClass.getResource("/coverage.xml").toURI).toString
+
+    val outputFile = File.createTempFile("transform-xml-output", ".xml").toString
+    mojo.outputXmlPath = outputFile
+
+    mojo.xpath = "//class[contains(@filename,'.scala')]"
+    mojo.action = "DELETE"
 
     mojo.execute()
 
+    println(Files.readAllLines(Paths.get(outputFile)))
 
+    val xml = XML.loadFile(outputFile)
+    val scalaClasses: NodeSeq = xml \\ "class" filter (_.attribute("filename").exists(_.text contains ".scala"))
+
+    scalaClasses should be(empty)
   }
 
 }
