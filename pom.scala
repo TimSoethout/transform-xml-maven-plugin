92103025bc94c101d6260949ea0fbc97d5508903
Tim Soethout <Tim.Soethout@ing.nl>
2015-06-01 15:53:11 +0200

    Cleanup of pom

--- pom.scala
+++ pom.scala
@@ -1,4 +1,5 @@
 import org.sonatype.maven.polyglot.scala.model._
+
 import scala.collection.immutable.Seq
 
 implicit val scalaVersion = ScalaVersion("2.11.6")
@@ -6,7 +7,7 @@ implicit val scalaVersion = ScalaVersion("2.11.6")
 ScalaModel(
   "nl.timmybankers.maven" % "transform-xml-maven-plugin" % "1.0-SNAPSHOT",
   name = "Transform XML Maven Plugin",
-  packaging= "maven-plugin",
+  packaging = "maven-plugin",
   contributors = Seq(
     Contributor(
       name = "Tim Soethout"
@@ -14,19 +15,9 @@ ScalaModel(
       //      organizationUrl = ""
     )
   ),
-  //  repositories = Seq(
-  //    Repository(
-  //      snapshots = RepositoryPolicy(enabled = false),
-  //      id = "sonatype-public-grid",
-  //      url = "http://repository.sonatype.org/content/groups/sonatype-public-grid/"
-  //    )
-  //  ),
+  description = "Maven plugin which can transform xml during the build. Currently supports deleting of nodes by xpath.",
+  scm = Scm(url = "https://github.com/TimSoethout/transform-xml-maven-plugin"),
   dependencies = Seq(
-    //    "io.tesla.polyglot" % "tesla-polyglot-common" % "0.0.1-SNAPSHOT",
-    //    "com.twitter" %% "util-eval" % "6.3.8",
-    //    "com.googlecode.kiama" %% "kiama" % "1.5.1",
-    //    "org.specs2" %% "specs2" % "2.1.1" % "test",
-    //    "junit" % "junit" % "4.12" % "test"
     "org.apache.maven.plugin-tools" % "maven-plugin-tools-api" % "3.4",
     "org.apache.maven.plugin-tools" % "maven-plugin-annotations" % "3.4",
     "org.scala-lang.modules" %% "scala-xml" % "1.0.3",
@@ -42,9 +33,6 @@ ScalaModel(
 
       )
     )
-//    tasks = Seq(Task("someTaskId", "verify") {
-//      ec => println("I'm Scala running during the verify phase. The ec passed in allows me to access the project")
-//    })
   ),
   modelVersion = "4.0.0"
 )
